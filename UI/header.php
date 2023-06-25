55716ec40e217b1faaa2e4e0f641bba8025449ad
Neokyuubi <40678329+neokyuubi@users.noreply.github.com>
2023-06-25 05:47:23 +0100

    local Css and JS and Font

--- UI/header.php
+++ UI/header.php
@@ -9,14 +9,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="UI/assets/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="UI/assets/jquery-3.7.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>  
+    <script src="UI/assets/materialize.min.js"></script>  
     <link rel="stylesheet" href="UI/assets/style.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="UI/assets/sweetalert2@11"></script>
+    <link href="UI/assets/icon.css" rel="stylesheet">
     <script src="UI/assets/script.js"></script>
     <title>Project Manager</title>
 </head>
